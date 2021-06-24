a09fdc90104614d152623b271435ac009a15495d
Abena Serwaa Johene Amo <ecn19seu@uea.ac.uk>
2021-06-24 13:09:30 +0000

    Added the articles and rendered the first article.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 
 import './App.css';
+import React, { Component } from 'react';
+import data from "./data/article-1.json";
+const title = data.title;
+
+const body = data.body
 
 function App() {
   return (
@@ -7,6 +12,7 @@ function App() {
       <header className="App-header">
           <h3> BBC News Article</h3>
       </header>
+        {title}
     </div>
   );
 }
