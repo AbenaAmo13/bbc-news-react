df9ebb93e58723fed79c2f366b9b13bf08c61db1
Abena Serwaa Johene Amo <ecn19seu@uea.ac.uk>
2021-06-26 22:03:33 +0000

    added image to card

--- src/article1.js
+++ src/article1.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import data from "./data/article-1.json";
-import {Card} from "react-bootstrap";
+import {Card, CardImg} from "react-bootstrap";
+import image from './image.jpg'
 
 const title = data.title;
 
 const body = data.body
+
 const bodyModel = body.map((bodyModelDetails, key)=>{
     return(
 
             bodyModelDetails.model
     )})
-
 
 
 
@@ -23,30 +24,9 @@ class Article1 extends Component{
                         <Card.Title>
                             {title}
                         </Card.Title>
-                        {body.map((bodyDetail, key)=>{
-                        return (
-                            <div key={0}>
-                            {
-                                bodyDetail.type
-                            }
-                                {bodyModel.map((bodyDetail, key)=>{
-                                    return(
-                                        <div key={key}>
-                                            {
-                                                bodyDetail.text
-
-                                            }
-                                        </div>
-
-                                    );
-                                })}
-                        </div>
-
-                        );
-                    })}
+                    <CardImg src={bodyModel[2].url}/>
                        <div>
-                           {body[5].type}
-                           {bodyModel[5].type}
+
                        </div>
 
 
