6d7805c228151e3af9d276d9d33b37fff064568c
Abena Serwaa Johene Amo <ecn19seu@uea.ac.uk>
2021-06-26 22:46:26 +0000

    Commented all the code for article1.js

--- src/article1.js
+++ src/article1.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+/* import the article 1 json file as data to be able to get the information from it */
 import data from "./data/article-1.json";
+/* Import card so you will be able to use the card component*/
 import {Card, CardDeck, CardImg} from "react-bootstrap";
-
-
-
+/*get the title key from the json file and put it in a const named title*/
 const title = data.title;
 
+/*get the body array from the artile1-json and store it in a const*/
 const body = data.body
 
+/*Get access to all the keys inside the model key*/
 const bodyModel = body.map((bodyModelDetails, key)=>{
     return(
 
@@ -21,6 +23,8 @@ class Article1 extends Component{
         return(
             <div>
                 <Card>
+
+                    {/* Get the image from the json article file and render it, with the appropriate height and width. */}
                     <CardImg src={bodyModel[2].url} height={bodyModel[2].height} width={bodyModel[2].width}/>
                     <Card.Body>
                         <Card.Title>
@@ -28,12 +32,14 @@ class Article1 extends Component{
                         </Card.Title>
                     <Card.Header>
                         <i>
+                            {/* Render the heading from the article.json file */}
                             {bodyModel[0].text}
                         </i>
 
                     </Card.Header>
                         <Card.Text>
                             <p>
+                                {/* Render the paragraphs from the article.json file*/}
                                 {bodyModel[1].text}
                             </p>
                             <p>
@@ -43,6 +49,7 @@ class Article1 extends Component{
                                 {bodyModel[4].text}
                             </p>
                             <p>
+                                {/* Render the list items in the article.json file*/}
                                 <li>
                                     {bodyModel[5].items[0]}
                                 </li>
@@ -52,18 +59,9 @@ class Article1 extends Component{
 
 
                             </p>
-
-
                         </Card.Text>
-
-
                        <div>
-
                        </div>
-
-
-
-
                     </Card.Body>
                 </Card>
 
